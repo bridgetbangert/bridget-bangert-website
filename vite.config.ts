f179ba638bafcb3fe8890b422b13a8de67c15e1e
Bridget Bangert <bridgetmbangert@gmail.com>
2025-03-04 18:12:30 -0600

    Add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite-deploy-demo/',
+  base: '/bridget-bangert-website/',
   plugins: [
     react(),
     tailwindcss(),
